f0cb6a8be45716b66f6570f1eb6e15174e876678
Hyuck <lhg1304@nexmore.co.kr>
2021-05-24 18:21:22 +0900

    Ch 06. 뽀모도로 타이머 - 04. 완성도 높이기

--- AopPart2Chapter06/app/src/main/java/com/example/chapter06/MainActivity.kt
+++ AopPart2Chapter06/app/src/main/java/com/example/chapter06/MainActivity.kt
@@ -56,14 +56,17 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                    currentCountDownTimer?.cancel()
-                    currentCountDownTimer = null
+                    stopCountDown()
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
                     seekBar ?: return
 
-                    startCountDown()
+                    if (seekBar.progress == 0) {
+                        stopCountDown()
+                    } else {
+                        startCountDown()
+                    }
                 }
 
             }
@@ -98,6 +101,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun stopCountDown() {
+        currentCountDownTimer?.cancel()
+        currentCountDownTimer = null
+        soundPool.autoPause()
+    }
+
     private fun completeCountDown() {
         updateRemainTime(0)
         updateSeekBar(0)
@@ -111,7 +120,7 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     private fun updateRemainTime(remainMillis: Long) {
         val remainSeconds = remainMillis / 1000
-        remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainMinutesTextView.text = "%02d'".format(remainSeconds / 60)
         remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
     }
 
