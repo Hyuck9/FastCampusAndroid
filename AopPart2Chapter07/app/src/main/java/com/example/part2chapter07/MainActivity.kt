3812f10cfb3c1a808d9188cd6d596bcf2aec82b1
Hyuck <lhg1304@nexmore.co.kr>
2021-05-24 18:50:10 +0900

    Ch 07. 녹음기 - 02. 권한 요청하기

--- AopPart2Chapter07/app/src/main/java/com/example/part2chapter07/MainActivity.kt
+++ AopPart2Chapter07/app/src/main/java/com/example/part2chapter07/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.part2chapter07
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
     private val recordButton: RecordButton by lazy { findViewById(R.id.recordButton) }
+    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
 
     private var state = State.BEFORE_RECORDING
 
@@ -13,11 +16,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestAudioPermission()
         initViews()
     }
 
-    fun initViews() {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if (!audioRecordPermissionGranted) {
+            finish()
+        }
+    }
+
+    private fun requestAudioPermission() {
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
+
+    private fun initViews() {
         recordButton.updateIconWithState(state)
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 
 }
