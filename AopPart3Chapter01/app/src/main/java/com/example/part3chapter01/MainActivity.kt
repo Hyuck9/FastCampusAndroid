6338d5d0068e78b5671531103e0ff6bb7dbb69c8
Hyuck <lhg1304@nexmore.co.kr>
2021-05-27 19:48:07 +0900

    Ch 01. 푸시 알림 수신기 - 06. Notification 구현하기 - 2

--- AopPart3Chapter01/app/src/main/java/com/example/part3chapter01/MainActivity.kt
+++ AopPart3Chapter01/app/src/main/java/com/example/part3chapter01/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.part3chapter01
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_main)
 
 		initFirebase()
+		updateResult()
+	}
+
+	override fun onNewIntent(intent: Intent?) {
+		super.onNewIntent(intent)
+
+		setIntent(intent)
+		updateResult(true)
 	}
 
 	private fun initFirebase() {
@@ -24,5 +34,15 @@ class MainActivity : AppCompatActivity() {
 					firebaseTokenTextView.text = task.result
 				}
 			}
+	}
+
+	@SuppressLint("SetTextI18n")
+	private fun updateResult(isNewIntent: Boolean = false) {
+		resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+		if (isNewIntent) {
+			"(으)로 갱신했습니다."
+		} else {
+			"(으)로 실행했습니다."
+		}
 	}
 }
