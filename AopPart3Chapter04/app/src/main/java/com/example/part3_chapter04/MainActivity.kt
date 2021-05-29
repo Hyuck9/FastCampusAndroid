a3392e4450cc06abca00081557f368d49ecf1ced
Hyuck <lhg1304@gmail.com>
2021-05-30 01:51:03 +0900

    Ch 04. 도서 리뷰 앱 - 02. 도서 리스트 화면 - Open API 를 통해 도서 목록 가져오기

--- AopPart3Chapter04/app/src/main/java/com/example/part3_chapter04/MainActivity.kt
+++ AopPart3Chapter04/app/src/main/java/com/example/part3_chapter04/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.part3_chapter04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.part3_chapter04.api.BookService
+import com.example.part3_chapter04.model.BestSellerDto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		val retrofit = Retrofit.Builder()
+			.baseUrl("https://book.interpark.com")
+			.addConverterFactory(GsonConverterFactory.create())
+			.build()
+
+		val bookService = retrofit.create(BookService::class.java)
+
+		bookService.getBestSellerBooks("D5DB4760E780540FA32AE026036C98987EDEDACB0D25A4AB47E92F033A3870E3")
+			.enqueue(object : Callback<BestSellerDto> {
+				override fun onResponse(
+					call: Call<BestSellerDto>,
+					response: Response<BestSellerDto>
+				) {
+					// TODO: 성공처리
+					if (response.isSuccessful.not()) {
+						Log.e(TAG, "NOT!! SUCCESS")
+						return
+					}
+					response.body()?.let {
+						Log.d(TAG, it.toString())
+
+						it.books.forEach { book ->
+							Log.d(TAG, book.toString())
+						}
+					}
+				}
+
+				override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
+					// TODO: 실패처리
+					Log.e(TAG, t.toString())
+				}
+
+			})
+	}
+
+	companion object {
+		private const val TAG = "MainActivity"
 	}
 }
