5a31c5b628f6dab46ba17e5f4b975ab751410d1b
Hyuck <lhg1304@nexmore.co.kr>
2022-09-25 17:39:46 +0900

    Initialize - Ch 07. 영화 평점 서비스

---
+++ AopPart5Chapter07/app/src/main/java/com/example/aop/part5/aop/part5/aop/part5/aop/part5/chapter07/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aop.part5.aop.part5.aop.part5.aop.part5.chapter07
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
